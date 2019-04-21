5273b5b013b2a6d1a40e79abab10463f82c31329
chun-hung <billy0210828@gmail.com>
2019-04-21 18:11:03 -0400

    state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
 import TodoItem from "../TodoItem"
-import ContactCard from "../ContactCard"
+//import ContactCard from "../ContactCard"
 
 class App extends Component {
-  render() {
-	return (
-	  <React.Fragment>
-        <div className="todo-list">
-	      <TodoItem />
-	      <TodoItem />
-	    </div> 
-        <div className="contact-card">
-          <ContactCard 
-			name="John"
-			phone="(240)521-1111"
-		    email="abcde@gmail.com"  
-		  />
-          <ContactCard 
-			name="Susan"
-			phone="(240)333-1111"
-		    email="susan@gmail.com"  
-  		  />
-        </div>
-	  </React.Fragment>
-    );
-  }
+	constructor() {
+		super()
+		this.state = {
+			count: 0
+		}
+		this.plusClick = this.plusClick.bind(this)
+		this.minusClick = this.minusClick.bind(this)
+	}
+	//method in the class
+	plusClick() {
+		this.setState(prevState => {
+			return {count: prevState.count + 1}
+		})
+	}
+	minusClick() {
+		this.setState(prevState => {
+			return {count: prevState.count - 1}
+		})
+	}
+	render() {
+		return (
+			<React.Fragment>
+				<div className="todo-list">
+	      			<TodoItem />
+	      			<TodoItem />
+	    		</div> 
+				<hr />
+        		<div className="state">
+  		  			<h1>{this.state.count}</h1>
+					<button onClick={this.plusClick}> Puls one!! </button>
+					<button onClick={this.minusClick}> Minus one!! </button>
+				</div>
+				<hr />
+
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
