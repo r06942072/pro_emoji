acc3f3817de9168aa5da0d93066888fcc352d9bb
chun-hung <billy0210828@gmail.com>
2019-04-22 13:55:30 -0400

    Prettier on vim, see App/index.js

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react"
+import React, {Component} from 'react';
 import './style.css';
 
 class TodoItem extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			task: this.props.task,
-			isChecked: false
-		}
-		this.toggleCheckbox = this.toggleCheckbox.bind(this)
-	}
-	//method in the class
-	toggleCheckbox() {
-		this.setState({
-			isChecked: !this.state.isChecked
-    	});
-	}
-	/*
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: this.props.task,
+      isChecked: false,
+    };
+    this.toggleCheckbox = this.toggleCheckbox.bind(this);
+  }
+  //method in the class
+  toggleCheckbox() {
+    this.setState({
+      isChecked: !this.state.isChecked,
+    });
+  }
+  /*
 	//Only be called one time. Another way to setState instead of using constructor
 	componentDidMount() {
 		this.setState({
@@ -24,18 +24,19 @@ class TodoItem extends Component {
          });
 	}
 	*/
-	render() {
-		console.log("TodoItem render: " + this.state.isChecked);
-		return (
-			<div className={this.state.isChecked? "item-checked" :"item-unchecked"}>
-				<input type="checkbox" 
-					checked={this.state.isChecked}
-					onChange={this.toggleCheckbox}
-				/>
-				<p> {this.state.task} </p>
-			</div>
-		);
-	}
+  render() {
+    console.log('TodoItem render: ' + this.state.isChecked);
+    return (
+      <div className={this.state.isChecked ? 'item-checked' : 'item-unchecked'}>
+        <input
+          type="checkbox"
+          checked={this.state.isChecked}
+          onChange={this.toggleCheckbox}
+        />
+        <p> {this.state.task} </p>
+      </div>
+    );
+  }
 }
 
-export default TodoItem
+export default TodoItem;
